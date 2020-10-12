508a649fe75f75ceefdbc03e1d62c16ab299c12f
Zikai Cai <zikaicai@usc.edu>
2020-10-11 23:38:47 -0700

    Format document

--- behavior_recognition.py
+++ behavior_recognition.py
@@ -1,13 +1,15 @@
 import pandas as pd
 
+
 def rotate(i):
-    switcher={
-        0:'Up',
-        1:'Right',
-        2:'Down',
-        3:'Left'
+    switcher = {
+        0: 'Up',
+        1: 'Right',
+        2: 'Down',
+        3: 'Left'
     }
-    return switcher.get(i,"Invalid")
+    return switcher.get(i, "Invalid")
+
 
 df = pd.read_csv('./data/data_2_preprocessed.csv')
 
@@ -20,10 +22,12 @@ pickdrop_action = []
 diff_x = 0
 diff_z = 0
 
-for i in range(0,df.shape[0]-1):
-    diff_x = df.iloc[i+1]['player0_position_x'] - df.iloc[i]['player0_position_x']
-    diff_z = df.iloc[i+1]['player0_position_z'] - df.iloc[i]['player0_position_z']
-    
+for i in range(0, df.shape[0]-1):
+    diff_x = df.iloc[i+1]['player0_position_x'] - \
+        df.iloc[i]['player0_position_x']
+    diff_z = df.iloc[i+1]['player0_position_z'] - \
+        df.iloc[i]['player0_position_z']
+
     if (diff_x and diff_z):
         move_action.append(rotate(-1))
     elif (diff_x == 0 and diff_z == 0):
@@ -32,10 +36,10 @@ for i in range(0,df.shape[0]-1):
         move_action.append(rotate(2-diff_x))
     elif (diff_z):
         move_action.append(rotate(1-diff_z))
-        
-    cut_action.append(abs(df.iloc[i]['time']-df.iloc[i+1]['time'])>100)
+
+    cut_action.append(abs(df.iloc[i]['time']-df.iloc[i+1]['time']) > 100)
     pickdrop_action.append(False)
-    
+
 move_action.append(rotate(df.iloc[df.shape[0]-1]['player0_position_a']))
 cut_action.append(False)
 pickdrop_action.append(False)
@@ -43,5 +47,5 @@ df['move_action'] = move_action
 df['cut_action'] = cut_action
 df['pickdrop_action'] = pickdrop_action
 
-#print(df)
+# print(df)
 df.to_csv('./data/data_3_w_action.csv')
--- data_preprocessor.py
+++ data_preprocessor.py
@@ -9,12 +9,15 @@ z_border = z_base-step/2
 
 df = pd.read_csv('./data/data_1_separated.csv')
 
-df['player0_position_x'] = ((df['player0_position_x']-x_border)/step).astype(int)
-df['player0_position_z'] = ((df['player0_position_z']-z_border)/step).astype(int)
+df['player0_position_x'] = (
+    (df['player0_position_x']-x_border)/step).astype(int)
+df['player0_position_z'] = (
+    (df['player0_position_z']-z_border)/step).astype(int)
 df['player0_position_a'] = ((df['player0_position_a']+45)/90).astype(int)
 df.loc[df['player0_position_a'] == 4, ['player0_position_a']] = 0
 
-df = df.drop(columns=['player0_position_y', 'player1_position_x', 'player1_position_y', 'player1_position_z', 'player1_position_a'])
+df = df.drop(columns=['player0_position_y', 'player1_position_x',
+                      'player1_position_y', 'player1_position_z', 'player1_position_a'])
 
 #df['player1_position_x'] = ((df['player1_position_x']-x_border)/step).astype(int)
 #df['player1_position_z'] = ((df['player1_position_z']-z_border)/step).astype(int)
@@ -22,11 +25,11 @@ df = df.drop(columns=['player0_position_y', 'player1_position_x', 'player1_posit
 #df.loc[df['player1_position_a'] == 4, ['player1_position_a']] = 0
 
 duplicate_row = [True]
-for i in range(0,df.shape[0]-1):
-    duplicate_row.append(not all((df.iloc[i]==df.iloc[i+1]).values))
+for i in range(0, df.shape[0]-1):
+    duplicate_row.append(not all((df.iloc[i] == df.iloc[i+1]).values))
 df = df.loc[duplicate_row]
 
 df['time'] = df.index
 
-#print(df)
+# print(df)
 df.to_csv('./data/data_2_preprocessed.csv')
