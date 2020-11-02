5c06494d541dbdb9543daaa612714cf5806ed457
elliebababa <ellie24.huang@gmail.com>
2020-11-01 18:05:39 -0800

    tai nan le

--- agent_part/simplyfied_env_test/il_run_gail.py
+++ agent_part/simplyfied_env_test/il_run_gail.py
@@ -3,31 +3,88 @@ import logging
 import gym
 import numpy as np
 import tensorflow as tf
+import tensorflow.keras as K
+import tensorflow.keras.layers as kl
 import overcooked_gym_env
 from overcooked_ai_py.mdp.actions import Action
-from il_model_utils import load_from_json, NullContextManager, TfContextManager, build_model, save_model
-from traj_agent_ac import A2CAgent, Model
+from il_model_utils import load_from_json, NullContextManager, TfContextManager, TRAINING_PARAMS, build_model, save_model
+
 
 def argparser():
     parser = argparse.ArgumentParser()
     parser.add_argument('--gamma', default=0.95)
-    # parser.add_argument('--iteration', default=int(1e4))
+    parser.add_argument('--iteration', default=int(1e1))
     return parser.parse_args()
 
-class Discriminator:
-    def __init__(self, env):
-        self.env = env
+class Actor2Critic:
+    def __init__(self):
+        self.action_model = self.construct_network()
+        self.critic_model = self.construct_network()
+        self.action_model.compile(optimizer=K.optimizers.Adam(TRAINING_PARAMS["learning_rate"]))
+        self.critic_model.compile(optimizer=K.optimizers.Adam(TRAINING_PARAMS["learning_rate"]))
+        
+    def construct_network(self):
+        return tf.keras.Sequential([
+            kl.Input(self.state_dim),
+            kl.Conv2D(64, 3, padding='same', activation='relu'),
+            kl.Conv2D(64, 3, padding='same', activation='relu'),
+            kl.Flatten(),
+            kl.Dense(64, activation='relu'),
+            kl.Dense(self.action_dim, activation='softmax')
+        ])
+        
+
+class Discriminator(tf.keras.Model):
+    def __init__(self):
+        super(Discriminator, self).__init__()
+        self.layer_1 = kl.Dense( units=20, activation=tf.nn.leaky_relu, name='layer1')
+        self.layer_2 = kl.Dense(units=20, activation=tf.nn.leaky_relu, name='layer2')
+        self.layer_3 = kl.Dense(units=20, activation=tf.nn.leaky_relu, name='layer3')
+        self.layer_prob = kl.Dense(units=1, activation=tf.nn.sigmoid, name='prob')
+                                  
+    def get_prob(self, s_a):
+        x = self.layer_1(s_a)
+        x = self.layer_2(x)
+        x = self.layer_3(x)
+        return self.layer_prob(x)
+        
+    def get_rewards(self, s_a):
+        prob = self.get_prob(s_a)
+        return tf.math.log(prob) #clip?
     
+    def train_step(self, inputs):
+        with tf.GradientTape() as tape:
+            prob_expert = self.get_prob(inputs[0][0])
+            prob_agent = self.get_prob(inputs[0][1])
+            loss_expert = tf.reduce_mean(tf.math.log(prob_expert))
+            loss_agent = tf.reduce_mean(tf.math.log(1 - prob_agent))
+            loss = -(loss_agent + loss_expert)
+            grads = tape.gradient(loss, self.trainable_variables)
+            self.optimizer.apply_gradients(zip(grads, self.trainable_variables))
+        return {"loss": loss}
+
+
+def concat_s_a(s, a):
+    s = tf.reshape(s, shape=(s.shape[0], -1))
+    a = tf.one_hot(a, Action.NUM_ACTIONS)
+    a = tf.reshape(a, shape=(a.shape[0], -1))
+    return tf.concat([s,a], axis=-1)
+
 
 def train(args):
     env = overcooked_gym_env.get_gym_env(layout_name="cramped_room", horizon=400)
-    model = Model(num_actions=len(Action.ALL_ACTIONS))
-    agent = A2CAgent(gamma=args.gamma)
-    D = Discriminator(env)
+    # Agent
+    A2C = Actor2Critic()
+    # Discriminator
+    D = Discriminator()
+    D.compile(K.optimizers.Adam(TRAINING_PARAMS["learning_rate"]))
+    #
     obs = env.reset()
     success_num = 0
     #load expert observations and actions
-    expert_observations, expert_observations_from_pixels, expert_actions = load_from_json()
+    _, expert_observations, expert_actions = load_from_json()
+    expert_observations = expert_observations[:400,]
+    expert_actions = expert_actions[:400,]
 
     for iterations in range(args.iteration):
         observations = []
@@ -39,9 +96,8 @@ def train(args):
         run_policy_steps = 0
         while True:
             run_policy_steps += 1
-            act, v_pred = agent.model.action_value(obs[None, :])
-            act = np.asscalar(act)
-            v_pred = np.asscalar(v_pred)
+            act = A2C.get_action(obs[None, :])
+            v_pred = A2C.get_value(concat_s_a(obs, act))
 
             next_obs,reward,done,info = env.step(act)
 
@@ -71,18 +127,11 @@ def train(args):
             success_num = 0
 
         observations = np.asarray(observations)
-        # observations = np.reshape(observations,newshape=[-1] + list(ob_space.shape))
         actions = np.array(actions).astype(dtype = np.int32)
 
-        for i in range(2):
-            D.train(expert_s = expert_observations,
-                    expert_a = expert_actions,
-                    agent_s = observations,
-                    agent_a = actions)
+        D.fit([concat_s_a(expert_observations, expert_actions), concat_s_a(observations, actions)], epochs=2)
 
-
-        d_rewards = D.get_rewards(agent_s=observations,agent_a = actions)
-        d_rewards = np.reshape(d_rewards,newshape=[-1]).astype(dtype=np.float32)
+        d_rewards = D.get_rewards(concat_s_a(observations, actions))
 
         _, next_value = agent.model.action_value(next_obs[None, :])
         returns, advs = agent._returns_advantages_rl(rewards, dones, v_preds, next_value)
