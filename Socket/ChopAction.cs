d4da19f52a6e24d3ee82b1cf9952d34f46e66938
unknown <shengxia@usc.edu>
2020-11-09 13:31:46 -0800

    add chop actions

---
+++ Socket/ChopAction.cs
@@ -0,0 +1,30 @@
+﻿namespace Overcooked_Socket
+{
+
+    internal class ChopAction : Action {
+
+        private readonly PlayerControls player;
+        //private readonly ClientWorkstation workstation;
+
+        public ChopAction(PlayerControls player) {
+            this.player = player;
+            //this.workstation = workstation;
+            
+            Keyboard.Get().SendDown(Keyboard.Input.CHOP_THROW);
+            
+            Logger.Log("ChopAction instantiated");
+        }
+
+        public bool Update() {
+            //return StationUtil.HasFinishedChopping(workstation);
+            Keyboard.Get().SendUp(Keyboard.Input.CHOP_THROW);
+            return true;
+        }
+
+        public void End() {
+            Keyboard.Get().SendUp(Keyboard.Input.CHOP_THROW);
+        }
+
+    }
+
+}
