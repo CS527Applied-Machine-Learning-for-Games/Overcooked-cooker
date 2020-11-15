f962c6bf5598143304a9632ec62ef4aa6eb6482a
gzlong96 <30570937+gzlong96@users.noreply.github.com>
2020-11-14 20:45:08 -0800

    Fix chop

--- env/TrainEnv.py
+++ env/TrainEnv.py
@@ -13,6 +13,7 @@ class TrainEnv(Env.Env):
         prev_pos, cur_pos = [], []
         prev_hold, cur_hold = [], []
         prev_order, cur_order = [], []
+        prev_chopping, cur_chopping = 0, 0
 
         with open("../data/test.json", "w") as f:
             while True:
@@ -21,8 +22,9 @@ class TrainEnv(Env.Env):
                 cur_pos = self.getchefpos()
                 cur_hold = self.getchefholding()
                 cur_order = self.getorderlist()
+                cur_chopping = sum(self.getchopprogress())
 
-                if cur_pos != prev_pos or cur_hold != prev_hold or cur_order != prev_order:
+                if cur_pos != prev_pos or cur_hold != prev_hold or cur_order != prev_order or cur_chopping != prev_chopping:
                     new_state = {"pos": cur_pos, "hold": cur_hold, "order": cur_order, "objects": self.getobjposlist(),
                                  "score": self.getscore(), "pot": self.getpotprogress(), "chop": self.getchopprogress(),
                                  "fire": self.isfire()}
@@ -31,8 +33,9 @@ class TrainEnv(Env.Env):
                     prev_pos = copy.deepcopy(cur_pos)
                     prev_hold = copy.deepcopy(cur_hold)
                     prev_order = copy.deepcopy(cur_order)
+                    prev_chopping = cur_chopping
 
-                time.sleep(0.1)
+                time.sleep(0.5)
 
 
 if __name__ == '__main__':
